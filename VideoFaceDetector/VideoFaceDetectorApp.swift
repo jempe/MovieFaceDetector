13d0ce577751927f9656e157cd5916a37ba8d923
Jempe <info@jempe.org>
2023-03-13 19:26:40 -0500

    Initial Commit

---
+++ VideoFaceDetector/VideoFaceDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFaceDetectorApp.swift
+//  VideoFaceDetector
+//
+//  Created by Kastro on 13/3/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoFaceDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
